202b236e16fe26f78c83fe5180af45e7a9ee5fe0
Denis Papushaev <denqxotl@gmail.com>
2017-08-03 10:15:24 +0300

    Init

---
+++ src/main/java/ua/tascors/baseexitsapi/BaseExitsApiApplication.java
@@ -0,0 +1,12 @@
+package ua.tascors.baseexitsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseExitsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaseExitsApiApplication.class, args);
+	}
+}
